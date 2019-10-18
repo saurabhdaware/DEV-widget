30fac5925bd4712e467f3c86af44a942075c403f
Saurabh Daware <saurabhdaware99@gmail.com>
2019-10-19 00:43:38 +0530

    got javascript working

--- src/card.component.mjs
+++ src/card.component.mjs
@@ -1,27 +1,34 @@
 import {css} from './card.style.mjs';
 
+const htmlContent =  // html
+`
+<div class="header">
+
+</div>
+<div class="content">
+
+</div>
+`;
+
 // Throughout this class 'this' refers to the element <div-card />
 class DevCard extends HTMLElement{
     constructor(){
         super();
 
-        this.htmlContent =  // html
-        `
-        <div class="hi">ee haw</div>
-        `;
-
-
+        
 
         // Create template and set HTML and css and append it to shadow root
-        this.template = document.createElement('template');
-        this.template.innerHTML = `
-        <style>
-        ${css}
+        const template = document.createElement('template');
+        template.innerHTML = `<style>
+            ${css}
         </style>
-        ` + this.htmlContent;
+        <span class="devcard-htmlContent">
+            ${htmlContent}
+        </span>
+        `
 
         this._shadowRoot = this.attachShadow({ mode: 'open' });
-        this._shadowRoot.appendChild(this.template.content.cloneNode(true));
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
     // Methods from parent class HTMLElement.
@@ -42,10 +49,9 @@ class DevCard extends HTMLElement{
 
 
     // Custom Methods of this class
-    
+
     createCard(){
-        console.log(this.template);
-        this.template.innerHTML = '<h1>Yas</h1>'
+        this._shadowRoot.querySelector('.content').innerHTML = JSON.stringify(this.articles);
     }
 
     render(){
